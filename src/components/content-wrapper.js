f4a763a8da877cd0b0d692e15a5e2bcca7bc2f4a
Bratalix <bratalix@gmail.com>
2022-09-08 13:22:45 +1000

    Display none for media browser image title

--- src/components/content-wrapper.js
+++ src/components/content-wrapper.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import styled from '@emotion/styled';
+import React from "react";
+import styled from "@emotion/styled";
 
 const Wrapper = styled.section`
-  padding: 40px 15px;
-  margin-right: auto;
-  margin-left: auto;
-  @media (min-width: 768px) {
-    width: 750px;
-  }
-  @media (min-width: 992px) {
-    width: 970px;
-  }
-  @media (min-width: 1200px) {
-    width: ${props =>
-      props.width === 'wide'
-        ? '1170px'
-        : props.width === 'medium'
-        ? '970px'
-        : '750px'};
-  }
-  ${props => props.styles}
+	padding: 40px 15px;
+	margin-right: auto;
+	margin-left: auto;
+	@media (min-width: 768px) {
+		width: 750px;
+	}
+	@media (min-width: 992px) {
+		width: 970px;
+	}
+	@media (min-width: 1200px) {
+		width: ${(props) => (props.width === "wide" ? "1170px" : props.width === "medium" ? "970px" : "750px")};
+	}
+	.element-invisible {
+		display: none;
+	}
+	${(props) => props.styles}
 `;
 
-export default function ContentWrapper({children, width, styles}) {
-  return <Wrapper width={width} styles={styles}>{children}</Wrapper>;
+export default function ContentWrapper({ children, width, styles }) {
+	return (
+		<Wrapper width={width} styles={styles}>
+			{children}
+		</Wrapper>
+	);
 }
